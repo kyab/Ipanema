25c72eb3283a50293f5a9f06f6719823f34f7ec8
kyab <kyossi212@gmail.com>
2012-06-12 00:07:55 +0900

    fix typo

--- Ipanema/Song.rb
+++ Ipanema/Song.rb
@@ -15,8 +15,6 @@ mml ("o5l4 e4. c#8 c# <b8 >e2 c#8 c# <b4" <<
 	">d1^8 d#4 d8 c8 d4 c8 <a#4. >c8^1." << 
 	"f1^8 f#4 f8 d#8 f4 d#8 c#4. d#8^1." ) 
 
-	
-#TODO:
 chords <<-END
 	D69  -  D69  -  E7(9)  -  E7(9)  
 	Em7(9)  -  A7(13)  -  D69  -  Eb7(9)  
