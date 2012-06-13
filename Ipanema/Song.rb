cf99006ce1b13afb1ab1089cc89d8fd8eb80b7de
kyab <kyossi212@gmail.com>
2012-06-14 01:19:05 +0900

    add comments

--- Ipanema/Song.rb
+++ Ipanema/Song.rb
@@ -4,6 +4,8 @@ require "Player.rb"
 #----------------------The Main Script goes here-----------------------
 tempo(150)
 
+#track(1) do 
+
 # テキスト音楽「サクラ」のMML
 mml ("o5l4 e4. c#8 c# <b8 >e2 c#8 c# <b4" <<
 	 "o5l4 e4. c#8 c# <b8 >e2 c#8 c#4 <b4" <<
@@ -15,7 +17,9 @@ mml ("o5l4 e4. c#8 c# <b8 >e2 c#8 c# <b4" <<
 	">d1^8 d#4 d8 c8 d4 c8 <a#4. >c8^1." << 
 	"f1^8 f#4 f8 d#8 f4 d#8 c#4. d#8^1." ) 
 
+#end
 
+#track(2) do
 chords <<-END
 	D69  -  D69  -  E7(9)  -  E7(9)  
 	Em7(9)  -  A7(13)  -  D69  -  Eb7(9)  
@@ -26,6 +30,8 @@ chords <<-END
 	GbM7/Bb  -  GbM7/Bb  -  B7(13)  -  B7(13)  
 	
 	END
+
+#end
 
 onTick(0) do 
 	puts "song started"
